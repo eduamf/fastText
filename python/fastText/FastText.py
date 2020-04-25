df78e7782aa9d3c55469e69b2c267f152b87e905
Mello <eduardo.freitas@camara.leg.br>
2020-04-25 19:56:30 +0100

    Incremental learning transfer
    I was looking for a solution of the type proposed since the quality in the identification of the Portuguese language is very poor, being often confused with Spanish, Galician and Catalan. Although in some cases it can be confused, this should not affect the probability of being Portuguese as it is today. eric.x.sun (https://github.com/ericxsun) started this changes before but they were not accepted.

--- python/fastText/FastText.py
+++ python/fastText/FastText.py
@@ -340,6 +340,8 @@ def train_supervised(
     label="__label__",
     verbose=2,
     pretrainedVectors="",
+    incr=False,
+    inputModel="",
 ):
     """
     Train a supervised model and return a model object.
